ea2dddf534ba7251d6e0c9ee4f54ab34ba06e358
Minpur-ra <jordankarina46@gmail.com>
2026-02-06 11:34:49 -0300

    primeira aula - php

---
+++ lp3_aula_1/index.php
@@ -0,0 +1,21 @@
+ 
+ 
+ <h1>LP3 Aula 1</h1>
+
+ <?php
+$nome = "Karina";
+echo "Olá, mundo!" . $nome . "<br>";
+echo "Olá, mundo! $nome" . "<br>";
+$a = "6";
+$b = "4";
+$soma = $a + $b;
+$multi = $a * $b;
+$sub = $a - $b;
+$div = $a / $b;
+
+echo"A soma de $a + $b é: $soma " . "<br>";
+echo "A multiplicação de $a x $b é: $multi" . "<br>";
+echo "A subtração de $a - $b é: $sub" . "<br>";
+echo "A divisão de $a / $b é: $div";
+
+ ?>
