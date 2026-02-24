3d50981d878e8a79b854e732f85e7a2c642ad395
Minpur-ra <jordankarina46@gmail.com>
2026-02-24 17:30:23 -0300

    bootstrap

---
+++ lp3-aula-4/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aulita 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>teste uwu ;-;</h1>
+        <div class="row">
+            <div class="col-6">
+                <p>scrr</p>
+
+            </div>
+            <div class="col-2">
+
+                <p>scrr 2</p>
+
+            </div>
+            <div class="col-2">
+
+                <p>scrr 3</p>
+
+            </div>
+            <div class="col-2">
+
+                <p>scrr 4</p>
+
+            </div>
+</div>
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="carro.jpg" class="d-block w-100" height="1000px" width="auto" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="coelho.jpg" class="d-block w-100" height="1000px" width="auto" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="flo.jpg" class="d-block w-100" height="1000px" width="auto" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+
+            </div>
+        </div>
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  se nao clicar é gay
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">fds é gay do msm jeito KKKKK</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">fechar (sou gay therian beta)</button>
+        <button type="button" class="btn btn-primary">Salvar (sou hetero sigma boy)</button>
+      </div>
+    </div>
+  </div>
+</div></div>
+<button type="button" class="btn btn-success">
+    <i class="bi bi-airplane-engines"></i> clicou é gay</button>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
