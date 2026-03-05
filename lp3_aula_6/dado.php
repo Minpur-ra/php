3fd491a880961460d72a9d5d9637acebba806086
Minpur-ra <jordankarina46@gmail.com>
2026-03-05 17:33:14 -0300

    bd

---
+++ lp3_aula_6/dado.php
@@ -0,0 +1,15 @@
+
+
+
+
+<?php
+
+$email = $_POST['em'];
+$nome = $_POST['nm'];
+$texto = $_POST['tt'];
+
+
+echo $email . "<br>";
+echo $nome . "<br>";
+echo $texto . "<br>";
+?>
---
+++ lp3_aula_6/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aulita 6</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <h1>form</h1>
+            <form action="dado.php" method="POST">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">nome burrão ;-;</label>
+                    <input type="text" class="form-control" id="nome" 
+                        placeholder="miih booooohta 🫦🫦🫦🤭🤭 ;- ;-; ;;--;-;" name="nm">
+                    <label for="email" class="form-label">gmail animal ;-;</label>
+                    <input type="email" class="form-control" id="email"
+                        placeholder="aaaaaaih eu toh cum meeehdoh 🫦🫦;-;-;;-;-;-;-🤭🤭" name="em">
+                </div>
+                <div class="mb-3">
+                    <label for="txt" class="form-label">qq c acha q vem aq?
+                        ;-;--;-;-;;--;-;---;</label>
+                    <textarea class="form-control" id="txt" placeholder="euhh bohto ou naum boto 🫦🫦🫦🫦 ;-;-"
+                        rows="3" name="tt"></textarea>
+                </div>
+                <button type="submit" class="btn btn-danger">botou ;-;</button>
+                <button type="reset" class="btn btn-warning">desbotar ;---;-;-;--;</button>
+
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
